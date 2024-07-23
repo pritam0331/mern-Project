7bba726338f3e7d5625b9bc816ea4a32a128cb5a
akshitverse <akshitverma296@gmail.com>
2024-07-23 15:13:52 +0530

    About

--- projects/src/components/About.jsx
+++ projects/src/components/About.jsx
@@ -3,7 +3,29 @@ import './About.css'
 function About() {
   return (
     <div>
-    <h1>Why choose Rakt Daan gfgfg</h1>
+    <h1>Why choose Rakt Daan</h1>
+    <div className='container'>
+    <img src='https://www.shutterstock.com/image-vector/blood-collection-transfusion-icon-donor-600nw-2129911235.jpg' alt='Sample Collection'></img>
+    <div className="img1">
+        <p className="img1-text">Free & On-time Sample Collection</p>
+      </div>
+      <img src='https://www.shutterstock.com/image-vector/blood-test-tube-cartoon-vector-600nw-2431506631.jpg' alt='Free Report'></img>
+      <div className="img2">
+        <p className="img2-text">Free Report Counselling and Consultation</p>
+      </div>
+      <img src='https://i.pinimg.com/736x/2a/c0/ea/2ac0ea9be7924078b266c08ac6d7d3be.jpg' alt='Accurate Report'></img>
+      <div className="img3">
+        <p className="img3-text">Fast and Accurate Report</p>
+      </div>
+      <img src='https://static.vecteezy.com/system/resources/previews/004/897/637/non_2x/location-red-icon-simple-design-free-vector.jpg' alt='Franchise'></img>
+      <div className="img4">
+        <p className="img4-text">Franchise Opportunity</p>
+      </div>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPAw1KkGTDTqHgvDt3HI_23B2hteakfiT9Bg&s' alt='Satisfied Customers'></img>
+      <div className="img5">
+        <p className="img5-text">10M+ Satisfied Customers</p>
+      </div>
+    </div>
   </div>
   )
 }
