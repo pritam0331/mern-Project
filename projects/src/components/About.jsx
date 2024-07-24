936995efec492dadb6a5913e00dd6cbd870f1460
akshitverse <akshitverma296@gmail.com>
2024-07-24 13:53:14 +0530

    about

--- projects/src/components/About.jsx
+++ projects/src/components/About.jsx
@@ -3,27 +3,31 @@ import './About.css'
 function About() {
   return (
     <div>
-    <h1>Why choose Rakt Daan</h1>
+    <h1 className='head'>Why choose Rakt Daan</h1>
     <div className='container'>
-    <img src='https://www.shutterstock.com/image-vector/blood-collection-transfusion-icon-donor-600nw-2129911235.jpg' alt='Sample Collection'></img>
     <div className="img1">
-        <p className="img1-text">Free & On-time Sample Collection</p>
+    <img src='https://www.shutterstock.com/image-vector/blood-collection-transfusion-icon-donor-600nw-2129911235.jpg' alt='Sample Collection'></img>
+        <p>Free & On-time Sample Collection</p>
       </div>
+      <div className="img1">
       <img src='https://www.shutterstock.com/image-vector/blood-test-tube-cartoon-vector-600nw-2431506631.jpg' alt='Free Report'></img>
-      <div className="img2">
-        <p className="img2-text">Free Report Counselling and Consultation</p>
+        <p>Free Report Counselling and Consultation</p>
       </div>
+      <div className="img1">
       <img src='https://i.pinimg.com/736x/2a/c0/ea/2ac0ea9be7924078b266c08ac6d7d3be.jpg' alt='Accurate Report'></img>
-      <div className="img3">
-        <p className="img3-text">Fast and Accurate Report</p>
+        <p>Fast and Accurate Report</p>
       </div>
+      <div className="img1">
       <img src='https://static.vecteezy.com/system/resources/previews/004/897/637/non_2x/location-red-icon-simple-design-free-vector.jpg' alt='Franchise'></img>
-      <div className="img4">
-        <p className="img4-text">Franchise Opportunity</p>
+        <p>Franchise Opportunity</p>
       </div>
+      <div className="img1">
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPAw1KkGTDTqHgvDt3HI_23B2hteakfiT9Bg&s' alt='Satisfied Customers'></img>
-      <div className="img5">
-        <p className="img5-text">10M+ Satisfied Customers</p>
+        <p>10M+ Satisfied Customers</p>
+      </div>
+      <div className="img1">
+        <img src='https://media.istockphoto.com/id/954478248/vector/love-timer.jpg?s=612x612&w=0&k=20&c=XLRybEWTuZNmJ1Def7z6UV-ULz3pCOVvfTWgP-KT-rk=' alt='abc'></img>
+        <p>A vast inventory of more than 30000+</p>
       </div>
     </div>
   </div>
